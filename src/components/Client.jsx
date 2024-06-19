6e2a507361d0d48386992eec235235d90067d55b
TabindaNoor <tabindanoor415@gmail.com>
2024-06-19 21:53:27 +0500

    avatars

--- src/components/Client.jsx
+++ src/components/Client.jsx
@@ -1,11 +1,8 @@
-import React from 'react'
-// import Avatar from 'react-avatar'
 
 const Client = ({username}) => {
     console.log(username)
   return (
     <div>
-      {/* <Avatar name={username} /> */}
     </div>
   )
 }
