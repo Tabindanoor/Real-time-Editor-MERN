1f13384923c7d746ed28a8f4cf1851d9ac4d1b98
TabindaNoor <tabindanoor415@gmail.com>
2024-06-28 17:19:20 +0500

    done

--- server.js
+++ server.js
@@ -2,27 +2,10 @@ import express from 'express';
 import http from 'http';
 import { Server } from 'socket.io';
 import { Actions } from './Actions.js';
-// import path from 'path';
-// import { fileURLToPath } from 'url';
 
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
-
-// doing things for deployement
-// const __filename = fileURLToPath(import.meta.url);
-// const __dirname = path.dirname(__filename);
-
-// // Serve static files from the 'dist' directory
-// app.use(express.static(path.join(__dirname, 'dist')));
-
-// // Serve index.html for all other routes
-// app.get('*', (req, res) => {
-//     res.sendFile(path.join(__dirname, 'dist', 'index.html'));
-// });
-
-
-
 const userSocketMap = {};
 
 function getAllConnectedClients(roomId) {
@@ -33,23 +16,12 @@ function getAllConnectedClients(roomId) {
         })
     );
 }
-
 io.on('connection', (socket) => {
     console.log('socket connected', socket.id);
 
     socket.on(Actions.JOIN, ({ roomId, username }) => {
        console.log(roomId,"room id")
 
-        // const existingSocketId = Object.keys(userSocketMap).find(
-        //     (key) => userSocketMap[key] === username
-        // );
-
-        // // If the user already exists, remove the old socket and join with the new one
-        // if (existingSocketId) {
-        //     console.log(`Removing old socket ${existingSocketId} for user ${username}`);
-        //     io.sockets.sockets.get(existingSocketId).leave(roomId);
-        //     delete userSocketMap[existingSocketId];
-        // }
 
         console.log(roomId,"room id generated")
         userSocketMap[socket.id] = username;
@@ -63,13 +35,6 @@ io.on('connection', (socket) => {
             });
         });
     });
-
-
-    
-   
-
-
-    
 
 
     socket.on(Actions.CODE_CHANGE, ({ roomId, code }) => {
@@ -103,9 +68,38 @@ io.on('connection', (socket) => {
     });
 
     
-
-  
 });
 
 const PORT =  3000;
 server.listen(PORT, () => console.log(`Listening on port ${PORT}`));
+
+
+// doing things for deployement
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = path.dirname(__filename);
+
+// // Serve static files from the 'dist' directory
+// app.use(express.static(path.join(__dirname, 'dist')));
+
+// // Serve index.html for all other routes
+// app.get('*', (req, res) => {
+//     res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+// });
+
+
+
+
+        // const existingSocketId = Object.keys(userSocketMap).find(
+
+        //     (key) => userSocketMap[key] === username
+        // );
+
+        // // If the user already exists, remove the old socket and join with the new one
+        // if (existingSocketId) {
+        //     console.log(`Removing old socket ${existingSocketId} for user ${username}`);
+        //     io.sockets.sockets.get(existingSocketId).leave(roomId);
+        //     delete userSocketMap[existingSocketId];
+        // }
+
+        // import path from 'path';
+// import { fileURLToPath } from 'url';
